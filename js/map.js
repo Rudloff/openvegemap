e3e52433bf1929f5dfa4bbf105b3631b6601001b
Pierre Rudloff <contact@rudloff.pro>
2016-10-22 18:03:07 +0200

    Load tiles over HTTPS

--- js/map.js
+++ js/map.js
@@ -74,7 +74,7 @@ var openvegemap = (function () {
                 }
             );
 
-            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
