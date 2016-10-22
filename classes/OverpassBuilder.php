609e5d3ff19d8e5be648bd13dc5372defc0492e4
Pierre Rudloff <contact@rudloff.pro>
2016-10-22 15:03:49 +0000

    Applied fixes from StyleCI

--- classes/OverpassBuilder.php
+++ classes/OverpassBuilder.php
@@ -1,17 +1,18 @@
 <?php
+
 namespace OpenVegeMap;
 
 class OverpassBuilder extends \KageNoNeko\OSM\Query\OverpassBuilder
 {
-
-    public function whereTagStartsWith($tag, $element = null) {
+    public function whereTagStartsWith($tag, $element = null)
+    {
         $element = $this->assertElementWheres($element);
         $type = 'TagExpression';
-        $operator = "~";
-        $value = ".";
+        $operator = '~';
+        $value = '.';
         $tag = '^'.$tag.':.*$';
         $this->wheres[$element][$type][] = compact('tag', 'operator', 'value');
+
         return $this;
     }
-
 }
--- classes/OverpassGrammar.php
+++ classes/OverpassGrammar.php
@@ -1,17 +1,18 @@
 <?php
+
 namespace OpenVegeMap;
 
 class OverpassGrammar extends \KageNoNeko\OSM\Query\Grammars\OverpassGrammar
 {
-
-    protected function compileWhereTagExpression(array $where) {
+    protected function compileWhereTagExpression(array $where)
+    {
         $ql = [];
         foreach ($where as $whereTag) {
             $tag = addslashes($whereTag['tag']);
             $value = addslashes($whereTag['value']);
             $ql[] = "[~\"{$tag}\"{$whereTag['operator']}\"{$value}\"]";
         }
+
         return implode('', $ql);
     }
-
 }
