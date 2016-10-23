d2690792c4986b57a0119a07c78360b5a7d27d78
Pierre Rudloff <contact@rudloff.pro>
2016-10-23 13:19:52 +0000

    Applied fixes from StyleCI

--- editor/index.php
+++ editor/index.php
@@ -9,10 +9,11 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new \Slim\App();
 $container = $app->getContainer();
 $container['view'] = function ($c) {
-    $view = new \Slim\Views\Smarty(__DIR__.'/../templates/', ['compileDir'=>__DIR__.'/../templates_c/']);
+    $view = new \Slim\Views\Smarty(__DIR__.'/../templates/', ['compileDir' => __DIR__.'/../templates_c/']);
     $smartyPlugins = new \Slim\Views\SmartyPlugins($c['router'], $c['request']->getUri());
     $view->registerPlugin('function', 'path_for', [$smartyPlugins, 'pathFor']);
     $view->registerPlugin('function', 'base_url', [$smartyPlugins, 'baseUrl']);
+
     return $view;
 };
 $app->get('/{id}', function (Request $request, Response $response) use ($container) {
@@ -22,13 +23,13 @@ $app->get('/{id}', function (Request $request, Response $response) use ($contain
         $response,
         'edit.html',
         [
-            'properties'=>$feature->getProperties(),
-            'coords'=>$feature->getGeometry()->getCoordinates(),
-            'id'=>$feature->getId(),
-            'editProperties'=>[
-                'diet:vegan'=>'Vegan',
-                'diet:vegetarian'=>'Vegetarian'
-            ]
+            'properties'     => $feature->getProperties(),
+            'coords'         => $feature->getGeometry()->getCoordinates(),
+            'id'             => $feature->getId(),
+            'editProperties' => [
+                'diet:vegan'      => 'Vegan',
+                'diet:vegetarian' => 'Vegetarian',
+            ],
         ]
     );
 });
