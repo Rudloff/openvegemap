f27820aac1c143c9fa7e243966454dc02e283bac
Pierre Rudloff <contact@rudloff.pro>
2016-10-23 19:04:42 +0000

    Applied fixes from StyleCI

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -1,23 +1,25 @@
 <?php
 /**
- * ApiController class
+ * ApiController class.
  */
 namespace OpenVegeMap\Controller;
 
 use KageNoNeko\OSM\BoundingBox;
 use OpenVegeMap\OsmApi;
-use Slim\Http\Response as Response;
 use Slim\Http\Request as Request;
+use Slim\Http\Response as Response;
 
 /**
- * Controller for the GeoJSON API
+ * Controller for the GeoJSON API.
  */
 class ApiController
 {
     /**
-     * Return features in the request bounding box
-     * @param  Request  $request  HTTP request
-     * @param  Response $response HTTP response
+     * Return features in the request bounding box.
+     *
+     * @param Request  $request  HTTP request
+     * @param Response $response HTTP response
+     *
      * @return Response JSON response
      */
     public function bbox(Request $request, Response $response)
